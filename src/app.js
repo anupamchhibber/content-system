c94dad18e01da6aecf30a8426364969fa5024f67
anupamchhibber <chhibberanupam18@gmail.com>
2026-04-28 21:41:15 +0530

    final backend with db connection

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+/*require('dotenv').config(); 
+
 const express = require('express');
 const { sequelize } = require('./models');
 
@@ -42,4 +44,30 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   //console.log('Server running on ${PORT}');
    console.log(`Server running on ${PORT}`);
+});*/
+
+require('dotenv').config();
+
+const express = require('express');
+const { sequelize } = require('./models');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend Running');
+});
+
+// DB AUTH CHECK
+sequelize.authenticate()
+  .then(() => console.log(" DB Authentication Success"))
+  .catch(err => console.error(" Auth Error:", err));
+
+// DB SYNC
+sequelize.sync({ alter: true })
+  .then(() => console.log(" DB Connected Successfully"))
+  .catch(err => console.error(" DB Error:", err));
+
+app.listen(3000, () => {
+  console.log("Server running on 3000");
 });
